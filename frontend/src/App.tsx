23b6d70e09e33eb42f31cbcd51d266941b746b9b
Harmony Tayler <153692183+harmonytayler@users.noreply.github.com>
2025-03-05 15:57:36 -0700

    Remove image.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,6 @@ function Welcome() {
 function Band({name, members, formed}: {name:string; members:string; formed:number;}) {
   return(
     <>
-      <img />
       <h2>{name}</h2>
       <h3>Original Members: {members}</h3>
       <h3>Formed: {formed}</h3>
